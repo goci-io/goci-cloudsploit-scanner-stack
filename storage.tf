42e58f970163043d9be501fe5d068e4b09737ded
etwillbefine <tim.gatzemeier@gmx.de>
2020-09-10 20:59:59 +0200

    terraform fmt

--- storage.tf
+++ storage.tf
@@ -1,13 +1,13 @@
-module "cloudfront" {
-  source    = "git::https://github.com/goci-io/aws-cloudfront-s3.git?ref=master"
-  namespace = var.namespace
-  name               = var.name
-  cloudfront_comment = "Cloudsploit Scan Results via Signed URLs"
-  cloudfront_min_ttl = 0
-  cloudfront_max_ttl = 0
-  cloudfront_default_ttl = 0
+module "cloudfront" {
+  source                            = "git::https://github.com/goci-io/aws-cloudfront-s3.git?ref=master"
+  namespace                         = var.namespace
+  name                              = var.name
+  cloudfront_comment                = "Cloudsploit Scan Results via Signed URLs"
+  cloudfront_min_ttl                = 0
+  cloudfront_max_ttl                = 0
+  cloudfront_default_ttl            = 0
   cloudfront_viewer_protocol_policy = "https-only"
-  cloudfront_public_keys = [tls_private_key.signing.public_key_pem]
+  cloudfront_public_keys            = [tls_private_key.signing.public_key_pem]
   lifecycle_expiration_rules = {
     cleanup = {
       prefix           = "/"
@@ -16,7 +16,7 @@ module "cloudfront" {
   }
 
   providers = {
-    aws = aws
+    aws         = aws
     aws.us-east = aws.us-east
   }
 }
