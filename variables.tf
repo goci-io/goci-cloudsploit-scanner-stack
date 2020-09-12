b56653af40a9957c3a3a80b82d3325545ccee433
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2020-09-12 18:17:29 +0200

    bump scanner default version 0.3.0

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "schedule" {
 
 variable "scanner_version" {
   type        = string
-  default     = "0.2.0"
+  default     = "0.3.0"
   description = "Desired Scanner Docker Image Tag"
 }
 
